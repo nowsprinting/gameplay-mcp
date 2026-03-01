0330e75a05baf35c8903081141aa76f34ad0f9e0
Koji Hasegawa <hasegawa@hubsys.co.jp>
2026-03-02 01:26:16 +0900

    Initial commit

---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("GameplayMcp.Editor")]
+[assembly: InternalsVisibleTo("GameplayMcp.Editor.Tests")]
+[assembly: InternalsVisibleTo("GameplayMcp.Tests")]
